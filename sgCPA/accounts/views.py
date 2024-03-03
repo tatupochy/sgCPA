8e42bed1410c032d1bdd0394b6b3ae767a422433
andresarevca <elias_carreras@fpuna.edu.py>
2024-03-03 20:51:22 -0300

    Add login and signup.

--- sgCPA/accounts/views.py
+++ sgCPA/accounts/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 # Create your views here.
+
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'  # Replace 'accounts/login.html' with the path to your login template
+
+    def form_valid(self, form):
+        # Custom logic to handle successful login
+        # For example, you can redirect the user to a specific page
+        print('Valid login attempt')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        # Custom logic to handle invalid login
+        # For example, you can log the failed login attempt
+        print('Invalid login attempt')
+        return super().form_invalid(form)
+
+
+class CustomSignupView(CreateView):
+    template_name = 'register.html'  # La plantilla de registro
+    form_class = UserCreationForm  # El formulario de creación de usuario proporcionado por Django
+    success_url = reverse_lazy('login')  # La URL a la que se redirigirá después del registro exitoso
+
+    def form_valid(self, form):
+        # Custom logic to handle successful registration
+        # For example, you can log the registered user
+        print('Valid registration attempt')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        # Custom logic to handle invalid registration
+        # For example, you can log the failed registration attempt
+        print('Invalid registration attempt')
+        print(form.errors.as_data())
+        return super().form_invalid(form)
+
--- sgCPA/sgCPA/urls.py
+++ sgCPA/sgCPA/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth import views as auth_views
+from accounts.views import CustomLoginView, CustomSignupView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/login/', CustomLoginView.as_view(), name='login'),
+    path('accounts/signup/', CustomSignupView.as_view(), name='signup'),
     # path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     # path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     # path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
