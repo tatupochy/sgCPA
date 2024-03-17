1b8062dbb444547be767f44860e4061d2c6dc5cf
=Juan Espinola <Julioespinola9999@gmail.com>
2024-03-16 21:13:42 -0300

    form validations frontend and backend with yup

--- sgCPA/students/models.py
+++ sgCPA/students/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=100)
     lastName = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, null=True, default=None)
     birthDate = models.DateField()
     inscriptionDate = models.DateField()
     ciNumber = models.CharField(max_length=20)
--- sgCPA/students/views.py
+++ sgCPA/students/views.py
@@ -1,29 +1,32 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.core.exceptions import ValidationError
+from django.http import HttpResponseBadRequest, HttpResponse
 from .models import Student
 
 def registrar_alumno(request):
-    if(request.method == "GET"):
-            return render(request, 'registrar_alumno.html')
-    else:        
-       form_data = request.POST.dict()
-
-       student = Student(
-            name=form_data.get('name', ''),
-            lastName=form_data.get('lastName', ''),
-            birthDate=form_data.get('birthDate', ''),
-            inscriptionDate=form_data.get('inscriptionDate', ''),
-            ciNumber=form_data.get('ciNumber', ''),
-            phone=form_data.get('phone', ''),
-            city=form_data.get('city', ''),
-            fatherPhone=form_data.get('fatherPhone', ''),
-            motherPhone=form_data.get('motherPhone', '')
-       )
-       
-       student.save()
-
+    if request.method == "GET":
+        return render(request, 'registrar_alumno.html')
     
-       return HttpResponse("enviado correctamente")
-      
-        
-        
+    form_data = request.POST.dict()
+
+    student = Student(
+        name=form_data.get('name'),
+        lastName=form_data.get('lastName'),
+        email=form_data.get('email'),
+        birthDate=form_data.get('birthDate'),
+        inscriptionDate=form_data.get('inscriptionDate'),
+        ciNumber=form_data.get('ciNumber'),
+        phone=form_data.get('phone'),
+        city=form_data.get('city'),
+        fatherPhone=form_data.get('fatherPhone'),
+        motherPhone=form_data.get('motherPhone')
+    )
+    
+    try:
+        student.full_clean()
+    except ValidationError as e:
+        errors = e.message_dict
+        return HttpResponseBadRequest("Error en la validación: {}".format(errors))
+
+    student.save()
+    return HttpResponse("Enviado correctamente")
