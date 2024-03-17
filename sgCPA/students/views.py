35edd3b52ea53549ce730b8d60c67c5f7664e854
=Juan Espinola <Julioespinola9999@gmail.com>
2024-03-16 21:22:48 -0300

    fixed merge error

--- sgCPA/students/views.py
+++ sgCPA/students/views.py
@@ -33,30 +33,6 @@ def registrar_alumno(request):
     student.save()
     return HttpResponse("Enviado correctamente")
 
-
-def registrar_alumno(request):    
-    if(request.method == "GET"):
-            return render(request, 'registrar_alumno.html')
-    else:        
-       form_data = request.POST.dict()
-
-       student = Student(
-            name=form_data.get('name', ''),
-            lastName=form_data.get('lastName', ''),
-            birthDate=form_data.get('birthDate', ''),
-            inscriptionDate=form_data.get('inscriptionDate', ''),
-            ciNumber=form_data.get('ciNumber', ''),
-            phone=form_data.get('phone', ''),
-            city=form_data.get('city', ''),
-            fatherPhone=form_data.get('fatherPhone', ''),
-            motherPhone=form_data.get('motherPhone', '')
-       )
-       
-       student.save()
-
-    
-       return HttpResponse("enviado correctamente")
-
 ###### Cursos ######   
 def registrar_curso(request):
     if request.method == 'POST':
