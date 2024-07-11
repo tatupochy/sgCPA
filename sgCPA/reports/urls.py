c2b0ef8b4123b28d6346bd97d4b921fd806b401c
Julio Espinola <90486971+Julio9999@users.noreply.github.com>
2024-07-10 23:11:48 -0400

    revenues_per_month

--- sgCPA/reports/urls.py
+++ sgCPA/reports/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('reports/', views.reports, name='reports'),
     path('reports/cursos_mas_ausencias/', views.report_cursos_mas_ausencias, name='report_cursos_mas_ausencias'),
     path('reports/download_pdf_report/', views.download_pdf_report, name='download_pdf_report'),
-    path('latePayments/', views.latePayments, name='latePayments'),
-    path('coursesRanking', views.coursesRanking, name='coursesRanking'),
+    path('reports/latePayments/', views.latePayments, name='latePayments'),
+    path('reports/coursesRanking', views.coursesRanking, name='coursesRanking'),
+    path('reports/revenues', views.revenues, name='revenues'),
+    path('reports/get_revenues_per_year/<int:year>', views.get_revenues_per_year, name='get_revenues_per_year')
 ]
--- sgCPA/reports/views.py
+++ sgCPA/reports/views.py
@@ -1,19 +1,21 @@
+import locale
 from django.shortcuts import render
-from django.db.models import Count,Min
+from django.db.models import Count,Min, Sum
 from django.db.models.functions import TruncMonth
 from attendances.models import AttendanceStudent
 from students.models import Course
 import calendar
 from datetime import datetime
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template.loader import render_to_string
 from xhtml2pdf import pisa
-import datetime
+
+
 
 import json
 
 
-from payments.models import EnrollmentDetail, Fee
+from payments.models import EnrollmentDetail, Fee, Payment
 from constans.paymentStates import StateEnum
 from students.models import Course, Student
 from django.db.models import Count
@@ -217,3 +219,73 @@ def coursesRanking(request):
 
 
     return render(request, 'coursesRanking.html', {'courses_list': courses_list})
+
+
+def revenues(request):
+    
+    
+    min_year = Course.objects.aggregate(Min('year'))['year__min']
+    
+    current_year = datetime.now().year
+    
+    years = list(range(min_year, current_year + 1))
+
+    data = {
+        'years': years
+    }
+
+    return render(request, 'revenues.html', data)
+
+
+# def get_revenues_per_year(request, year):
+
+#     # Filtrar los pagos por el año especificado y sumar el monto de los pagos
+#     total_revenue = Payment.objects.filter(year=year).aggregate(total=Sum('payment_amount'))['total']
+
+#     # Formatear el total de ingresos con separadores de miles
+#     formatted_total_revenue = "{:,.0f}".format(total_revenue).replace(",", ".")
+
+
+#     return JsonResponse({"year": year, "total_revenue": formatted_total_revenue})
+
+def get_revenues_per_year(request, year):
+    current_year = datetime.now().year
+    current_month = datetime.now().month
+
+    # Diccionario de equivalentes de meses en letras y en español
+    months_in_spanish = {
+        1: "Enero", 2: "Febrero", 3: "Marzo", 4: "Abril",
+        5: "Mayo", 6: "Junio", 7: "Julio", 8: "Agosto",
+        9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre"
+    }
+
+    # Filtrar pagos por año y calcular el total anual
+    total_revenue = Payment.objects.filter(year=year).aggregate(total=Sum('payment_amount'))['total']
+    if total_revenue is None:
+        total_revenue = 0
+
+    # Formatear el total anual con separadores de miles y sin decimales
+    formatted_total_revenue = int(total_revenue)
+
+    # Filtrar pagos por año y mes y calcular los totales mensuales
+    monthly_revenues = Payment.objects.filter(year=year).values('payment_date__month').annotate(total=Sum('payment_amount'))
+
+    # Crear un diccionario para almacenar las ganancias mensuales
+    monthly_revenues_dict = {month: 0 for month in range(1, 13)}
+    for revenue in monthly_revenues:
+        month = revenue['payment_date__month']
+        total = revenue['total']
+        monthly_revenues_dict[month] = int(total)
+
+    # Limitar los datos hasta el mes actual si el año es el actual
+    if year == current_year:
+        monthly_revenues_dict = {month: total for month, total in monthly_revenues_dict.items() if month <= current_month}
+
+    # Formatear las ganancias mensuales y cambiar los meses a letras en español
+    formatted_monthly_revenues = [(months_in_spanish[month], total) for month, total in monthly_revenues_dict.items()]
+
+    return JsonResponse({
+        "year": year,
+        "total_revenue": formatted_total_revenue,
+        "monthly_revenues": formatted_monthly_revenues
+    })
