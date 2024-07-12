42300535953210d1feb49d96e7d3b27b02bd7a46
Julio Espinola <90486971+Julio9999@users.noreply.github.com>
2024-07-11 20:57:37 -0400

    descargar pdf de ingresos totales

--- sgCPA/reports/static/scripts/revenues.js
+++ sgCPA/reports/static/scripts/revenues.js
@@ -1,41 +1,46 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const d = document;
-    const year = d.getElementById('year')
-    const revenues_table_body = d.getElementById('revenues-table-body')
-    const total_revenueElement = d.getElementById('total_revenue')
+// document.addEventListener('DOMContentLoaded', () => {
+//     const d = document;
+//     const year = d.getElementById('year')
+//     const revenues_table_body = d.getElementById('revenues-table-body')
+//     const total_revenueElement = d.getElementById('total_revenue')
+//     const revenues_table = d.getElementById('revenues-table')
 
-    year.addEventListener('change', async(e) => {
-        const value = e.target.value;
+//     year.addEventListener('change', async(e) => {
+//         const value = e.target.value;
 
-        const url = `get_revenues_per_year/${value}`
+//         const url = `get_revenues_per_year/${value}`
 
-        const response = await fetch(url)
+//         const response = await fetch(url)
 
-        const {monthly_revenues, total_revenue} = await response.json()
+//         const {monthly_revenues, total_revenue} = await response.json()
 
-        total_revenueElement.textContent = total_revenue;
-
-
-        const fragment = d.createDocumentFragment()
+//         total_revenueElement.textContent = total_revenue;
 
 
-        monthly_revenues.map(element => {
-            const tr = d.createElement('tr')
-            const tdMonth = d.createElement('td')
-            const tdRevenue = d.createElement('td')
-
-            tdMonth.textContent = element[0]
-            tdRevenue.textContent = element[1]
-
-            tr.append(tdMonth)
-            tr.append(tdRevenue)
-
-            fragment.append(tr)
-
-        });
-
-        revenues_table_body.append(fragment)
+//         const fragment = d.createDocumentFragment()
 
 
-    })
-})
+//         monthly_revenues.map(element => {
+//             const tr = d.createElement('tr')
+//             const tdMonth = d.createElement('td')
+//             const tdRevenue = d.createElement('td')
+
+//             tdMonth.textContent = element[0]
+//             tdRevenue.textContent = element[1]
+
+//             tr.append(tdMonth)
+//             tr.append(tdRevenue)
+
+//             fragment.append(tr)
+
+//         });
+
+//         revenues_table_body.innerHTML = '';
+
+//         revenues_table.innerHTML = '';
+
+//         revenues_table_body.append(fragment)
+
+
+//     })
+// })
